8cb02e3678cde905877f905bb017b2a3c6036d56
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-05-08 18:27:43 -0500

    First desing of the interfaces or mockups.

--- src/ui/AforeGUI.java
+++ src/ui/AforeGUI.java
@@ -1,5 +1,38 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+
 public class AforeGUI {
+	
+	
+	
+	public AforeGUI() {
+		
+	}
+
+	@FXML
+	private Pane mainPane;
+
+	@FXML
+	private TextField txtEmailLogin;
+
+	@FXML
+	private TextField txtPasswordLogin;
+	
+    @FXML
+    void buttonLogin(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void buttonRegister(ActionEvent event) {
+
+    }
+
+
 
 }
